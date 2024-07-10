c899d9953f4137fe30178176829c2a21fd3e98b6
NRG55 <maloj_1980@yahoo.fr>
2024-07-10 05:09:58 +0100

    Index.js - added an eventListener to the dropdown button (oncklick - opens or closes dropdown menu)and an eventListener to the window (if clicked anywhere else - closes dropdown menu). Added style.css file.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hey")
+const dropdownButton = document.querySelector(".dropdown-menu-button");
+
+dropdownButton.addEventListener("click", () => {
+    document.querySelector(".dropdown-menu").classList.toggle("visible");
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target !== dropdownButton) {
+        document.querySelector(".dropdown-menu").classList.remove("visible");
+    };
+});
